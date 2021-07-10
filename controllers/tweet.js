190fec4dbfd829bdfb18a3a448af4dc733d7c508
Apoorv <apoorvsingal0@gmail.com>
2021-07-10 22:13:38 +0530

    fix flat strings to role conversion

--- controllers/tweet.js
+++ controllers/tweet.js
@@ -1,32 +1,16 @@
 const Tweet = require("../models/Tweet.schema");
 const { parseRoles } = require("../parser");
-
-const normalize = (name_raw) => {
-  let name = "";
-  let state = 0;
-
-  for(let c of name_raw){
-    if(state == 0){
-      name += c.toUpperCase();
-      state = 1;
-      continue;
-    }
-    if(c == "-"){
-      name += " ";
-      state = 0;
-    } else {
-      name += c;
-    }
-  }
-  console.log(name_raw, name);
-};
+const roles = Object.values(require("../data/roles.json")).flatMap(roles => Object.keys(roles)).map(role => ({ [role.toLowerCase().replace(/\s+/g, "")]: role })).reduce((a, b) => ({ ...a, ...b }), {});
+const categories = Object.keys(require("../data/roles.json")).map(category => ({ [category.toLowerCase().replace(/\s+/g, "")]: category })).reduce((a, b) => ({ ...a, ...b }), {});
 
 exports.findAll = async ({ limit = 20, offset = 0, category, role, type, q }) => {
   const mongoQuery = { need_manual_verification: false };
 
+  console.log(role, roles[role])
+
   if(type) mongoQuery.type = type.toLowerCase();
-  if(category) mongoQuery.categories = normalize(category)
-  if(role) mongoQuery.roles = normalize(role);
+  if(category) mongoQuery.categories = categories[category]
+  if(role) mongoQuery.roles = roles[role];
 
   if(q){
     // Errors are to be handled in the catch block of the function calling this
