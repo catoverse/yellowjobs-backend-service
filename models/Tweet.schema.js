f075833647651c1c5619cb667f66ac358047fde4
Apoorv Singal <apoorvsingal0@gmail.com>
2021-09-05 18:43:27 +0530

    fix typo

--- models/Tweet.schema.js
+++ models/Tweet.schema.js
@@ -11,7 +11,7 @@ const schema = new mongoose.Schema(
 
     created_by: String,
     created_on: Number,
-    localtion: String,
+    location: String,
 
     need_manual_verification: {
       type: String,
